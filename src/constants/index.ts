27b9980a10ff48b5bf1069174c621fdb10a05202
itwillbeusedalot <86040413+itwillbeusedalot@users.noreply.github.com>
2025-03-09 00:15:38 +0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const ONE_MINUTE = 1000 * 60;
 export const FIVE_MINUTES = ONE_MINUTE * 5;
 export const ONE_HOUR = ONE_MINUTE * 60;
 
-export const WORKING_OPTIONS = [1, 15, 20, 25, 30, 45, 50].map((option) =>
+export const WORKING_OPTIONS = [1, 15, 20, 25, 30, 45, 50, 60].map((option) =>
   (option * ONE_MINUTE).toString()
 );
 
@@ -10,7 +10,7 @@ export const BREAK_OPTIONS = [1, 3, 5, 7, 10].map((option) =>
   (option * ONE_MINUTE).toString()
 );
 
-export const LONG_BREAK_OPTIONS = [1, 15, 20, 25, 30].map((option) =>
+export const LONG_BREAK_OPTIONS = [1, 10, 15, 20, 25, 30].map((option) =>
   (option * ONE_MINUTE).toString()
 );
 
